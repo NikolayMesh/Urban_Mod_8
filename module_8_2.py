002e1cdf659ecf031f26cd18162e08e96096d504
NikolayMesh <partizan.91@bk.ru>
2024-10-23 17:14:41 +0300

    add

---
+++ module_8_2.py
@@ -0,0 +1,34 @@
+def personal_sum(numbers):
+    result = 0
+    incorrect_data = 0
+
+    for item in numbers:
+        try:
+            result += item  # Пробуем преобразовать элемент в число
+        except (TypeError, ValueError) as exc:
+            print(f'Некорректный тип данных для подсчёта суммы - {item}')
+            incorrect_data += 1  # Увеличиваем счётчик некорректных данных
+
+    return result, incorrect_data
+
+
+def calculate_average(numbers):
+    if not isinstance(numbers, (list, str)):
+        print('В numbers записан некорректный тип данных')
+        return None
+
+    total_sum, incorrect_data = personal_sum(numbers)
+
+    try:
+        average = total_sum / (len(numbers) - incorrect_data)  # Вычисляем среднее
+    except ZeroDivisionError:
+        return 0  # Обработка деления на ноль
+
+    return average
+
+
+# Примеры вызова функции
+print(f'Результат 1: {calculate_average("1, 2, 3")}')  # Строка
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')  # Смешанные данные
+print(f'Результат 3: {calculate_average(567)}')  # Не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')  # Корректные данные
